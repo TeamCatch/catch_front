6640397226469b76af0ec7c1ecf1a7f059562b6c
Byunwoojin <sforot9@gmail.com>
2022-08-21 20:17:54 +0900

    feat: 파일업로드/이미지 촬영 선택 페이지

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,23 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { useRouter } from 'next/router';
+import { useRef } from 'react';
 
 const Home: NextPage = () => {
   const router = useRouter();
+  const file = useRef<HTMLInputElement>(null);
 
+  const handleInputFile = async (e: any) => {
+    console.log(e);
+    const files = e.target.files;
+    console.log(files);
+
+    let img_url = URL.createObjectURL(files[0]);
+    console.log(img_url);
+
+    const len = e.target.files.length;
+    router.push('/result');
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -21,54 +34,27 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          숙명여대 졸업 프로젝트{' '}
+          <code className={styles.code}>다빈, 수현, 우진 </code>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.button_cntr}>
+          <label className={styles.button} htmlFor="img_trash">
+            파일 업로드하기
+            <input
+              type="file"
+              id="img_trash"
+              accept="image/jpeg, image/jpg, image/png"
+              multiple={false}
+              ref={file}
+              onChange={handleInputFile}
+              style={{ width: '0', height: '0' }}
+            />
+          </label>
+          <div className={styles.button} onClick={() => router.push('/upload')}>
+            촬영하기
+          </div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-      <div onClick={() => router.push('/')}>이동</div>
     </div>
   );
 };
