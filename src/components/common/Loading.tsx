cb515a72196c85c16d82baff1e6ae50a902df783
Byunwoojin <sforot9@gmail.com>
2022-08-21 20:17:29 +0900

    feat: 로딩페이지

---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,56 @@
+import styled, { css } from 'styled-components';
+import React, { useState, useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
+
+const Loading = () => {
+  const loadingImg = useRef<HTMLDivElement>(null);
+
+  const [isLottie, setIsLottie] = useState(false);
+  useEffect(() => {
+    if (!isLottie && loadingImg.current) {
+      setIsLottie(true);
+      lottie.loadAnimation({
+        container: loadingImg.current,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        animationData: require('../../assets/lottie/loading.json'),
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <ModalContainer>
+        <div style={{ width: '8rem', height: '8rem' }} ref={loadingImg} />
+        <div style={{ fontSize: '1.6rem', fontWeight: '600' }}>
+          로딩 중입니다…
+        </div>
+      </ModalContainer>
+    </>
+  );
+};
+
+export default Loading;
+
+const ModalContainer = styled.div`
+  position: fixed;
+  z-index: 11;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  @media screen and (min-width: 480px) {
+    width: 432px;
+  }
+  @media screen and (max-width: 480px) {
+    width: 90%;
+  }
+
+  padding: 2.4rem 2rem;
+  padding-top: 3rem;
+  border-radius: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
