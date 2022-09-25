bbb1c8aaff2f2e2666edc072d5aa2aa130626a65
Byunwoojin <sforot9@gmail.com>
2022-09-25 18:43:03 +0900

    Revert "fix: 촬영 화면 모바일 오류 수정"
    This reverts commit f519610104c079b63e63249493ade88ffed0e971.

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -30,10 +30,9 @@ const UploadPage: NextPage = () => {
 const WebcamCapture = () => {
   const router = useRouter();
   const webcamRef = useRef<Webcam>(null);
-  const [imgSrc, setImgSrc] = useState<string | null | undefined>(null);
-  const [imgFile, setImgFile] = useState<MyFile | null>(null);
   const [isPC, setIsPC] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+
   let filter =
     'win16|win32|win64|wince|mac|macintel|macppc|mac68k|linux i686|linux armv7l|hp-ux|sunos';
 
@@ -50,11 +49,7 @@ const WebcamCapture = () => {
     setIsLoading(true);
     const imageSrc = webcamRef?.current?.getScreenshot();
     if (imageSrc) {
-      setImgSrc(imageSrc);
-      console.log(imageSrc);
       const file = dataURLtoFile(imageSrc, 'upload_img');
-      console.log(file);
-      setImgFile(file);
       onSubmitImageFile(file);
     }
   }, [webcamRef]);
@@ -64,6 +59,7 @@ const WebcamCapture = () => {
     height: isPC ? 400 : 2000,
     facingMode: 'environment',
   };
+  console.log(videoConstraints);
 
   useEffect(() => {
     let navi: any;
