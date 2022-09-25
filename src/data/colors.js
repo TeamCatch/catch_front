5002d2d0a8208b8bad80e5bfe88fe4517ebfa5b8
Byunwoojin <sforot9@gmail.com>
2022-09-25 17:24:10 +0900

    feat: 최종결과 화면 data 추가, UI 변경

---
+++ src/data/colors.js
@@ -0,0 +1,10 @@
+export const colors = [
+  { background: '#FFECE2', inner: '#E67701' },
+  { background: '#FFE9EC', inner: '#D84C6F' },
+  { background: '#F1F0FF', inner: '#794AEF' },
+  { background: '#D2E3FC', inner: '#1967D2' },
+  { background: '#FFECE2', inner: '#E67701' },
+  { background: '#FFE9EC', inner: '#D84C6F' },
+  { background: '#F1F0FF', inner: '#794AEF' },
+  { background: '#D2E3FC', inner: '#1967D2' },
+];
